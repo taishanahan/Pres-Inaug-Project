837f22f91864f3780bedc8f9c67f79dfec909051
Tai Shanahan <tps30@pitt.edu>
2023-04-15 11:55:43 -0400

    Dropdown test version

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,14 +1,12 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.div.content;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+var btn = document
+            .getElementsByClassName("collapsible");
+          
+        btn[0].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
